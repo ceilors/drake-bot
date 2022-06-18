a885ca40fd347f6ab551a17400fc80da448e4bb7
Alexey Golubev <dr.freecx@gmail.com>
2022-06-18 14:55:24 +0300

    chore: tiny update

--- response.py
+++ response.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.constants import ParseMode
+from telegram.constants import ParseMode, MessageEntityType
 
 import config
 import error
@@ -16,15 +16,21 @@ async def help(update: Update, context):
     await update.effective_chat.send_message(config.help_msg, parse_mode=ParseMode.HTML)
 
 
-def parse_msg(message, chat_type):
+def parse_msg(message, entities, chat_type):
     lines = []
     match chat_type:
         case "private":
             lines = [parse_line(l) for l in message.split("\n")]
         case "supergroup" | "group" | "channel":
-            hash_tag = "#drake"
-            if message.startswith(hash_tag):
-                lines = [parse_line(l) for l in message[len(hash_tag) :].strip().split("\n")]
+            if len(entities) == 0:
+                return []
+            entity = entities[0]
+            msg_hash_tag = message[entity.offset:entity.length]
+            if entity.type == MessageEntityType.HASHTAG and msg_hash_tag == "#drake":
+                message = message[entity.length:]
+                if len(message) == 0:
+                    raise error.ParseError("Где текст Билли?")
+                lines = [parse_line(l) for l in message.strip().split("\n")]
             else:
                 return []
     l = len(lines)
@@ -66,9 +72,11 @@ async def msg(update: Update, context):
     chat_type = update.effective_chat.type
 
     try:
-        # TODO: пофиксить данный костыль
-        text = update.message.text if update.message else update.channel_post.text
-        messages = parse_msg(text, chat_type)
+        if update.message is None and update.channel_post is None:
+            print(f"Something went wrong! update: {update}")
+            return
+        obj = update.message if update.message else update.channel_post
+        messages = parse_msg(obj.text, obj.entities, chat_type)
         if not messages:
             return
         if len(messages) > config.max_messages_count:
